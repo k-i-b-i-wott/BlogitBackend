c8b72cd306540e1a636ebb41a2b7d4cff43f92cf
Kibiwott <bk.kibiwott@gmail.com>
2025-04-11 16:49:47 +0300

    Initialized the backend

---
+++ index.js
@@ -0,0 +1,44 @@
+import {PrismaClient} from '@prisma/client'
+import express from "express"
+import bcrypt from "bcrypt";
+
+const app = express();
+const client = new PrismaClient();
+app.use(express.json())
+
+app.post('/auth/register',async (req,res)=>{
+    const {firstName,lastName,emailAddress,userName,password} = req.body
+    const hashedPassword = await bcrypt.hash(password,12);
+   try {
+    const user = await client.user.create({
+        data: {
+            firstName,
+            lastName,
+            emailAddress,
+            userName,
+            password:hashedPassword
+        }
+    })
+    res.status(200).json({
+        message:"User created successfully",
+        status:"Success",
+        data:user
+    })
+    
+   } catch (error) {
+    res.status(500).json({
+        message:"Error creating user",
+        status:"Failed",        
+    })
+   }
+    
+})
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000!')
+})
