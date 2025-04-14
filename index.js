ac60837c9bbe4dd07cc14c9c12a659dde70b2b03
Kibiwott <bk.kibiwott@gmail.com>
2025-04-14 14:34:07 +0300

    Solving deployment bugs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import {PrismaClient} from '@prisma/client'
+
+import {PrismaClient} from "@prisma/client"
 import express from "express"
 import bcrypt from "bcrypt";
 import { verifyUser } from './middlewares/register.usermiddleware.js';
