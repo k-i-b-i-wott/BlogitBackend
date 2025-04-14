e6a10b787de89b8a87f94ba0b9038916121cd6bb
Kibiwott <bk.kibiwott@gmail.com>
2025-04-14 13:10:08 +0300

    Completed the setup

--- index.js
+++ index.js
@@ -267,6 +267,8 @@ app.delete('/blog/post/:blogId',verifyUserInfo, async(req,res)=>{
     }
 
 })
-app.listen(3000, () => {
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
     console.log('Server running on port 3000!')
 })
